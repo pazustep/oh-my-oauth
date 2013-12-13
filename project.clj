cec96faf5bab1f25737d07cd0ed7adeea965b379
Marcus Brito <marcus@bri.to>
2013-12-13 14:52:04 -0200

    Rename project to "oh-my-auth"

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject ring-oauth-provider "0.1.0-SNAPSHOT"
+(defproject oh-my-auth "0.1.0-SNAPSHOT"
   :description "A ring middleware to support OAuth 1.0a authentication"
   :url "http://example.com/FIXME"
   :license {
