752ceb31d2745f9317a6140eecc3011eb51bcddb
Marcus Brito <marcus@bri.to>
2013-12-14 19:11:57 -0200

    Cleaner code, expanded configuration interface

--- src/oauth/middleware.clj
+++ src/oauth/middleware.clj
@@ -1,7 +1,7 @@
 (ns oauth.middleware
-  (:require [oauth.signature :as sig]
-            [clojure.string :as str]
-            [ring.util.codec :as codec]))
+  (:require [clojure.string :as str]
+            [ring.util.codec :as codec]
+            [oauth.signature :as sig]))
 
 (def ^:private signature-checkers
   {"hmac-sha1" sig/hmac-sha1-signature})
@@ -16,64 +16,174 @@
 (defn- required-params-missing? [params]
   (not-every? params required-params))
 
-(defn do-oauth
+;; The following validation functions are supposed to be chained together.
+;; Each can produce the expected outcome for the `authenticate-request`
+;; function or nil if further checks are necessary.
+
+(defn- validate-required-params
+  "Check if all required OAuth protocol parameters are present in the
+  request, returning an error if any required parameters are missing."
+  [params]
+  (let [missing (clojure.set/difference required-params params)]
+    (if-not (empty? missing)
+      [false {:error :missing-oauth-param
+              :status 400
+              :message (str "Missing required authentication parameter: " (str/join ", " missing))}])))
+
+(defn- validate-nonce
+  "Checks if the `oauth_nonce` value, together with `oauth_timestamp`,
+  `oauth_customer_key` and `oauth_token` values. The OAuth specification
+  says that a nonce should not be reused for the same timestamp, customer
+  key and token."
+  [{:strs [oauth_nonce oauth_timestamp oauth_customer_key oauth_token]} nonce-validator-fn]
+  (if-not (nonce-validator-fn oauth_nonce oauth_timestamp oauth_customer_key oauth_token)
+    [false {:error :invalid-nonce
+            :status 400
+            :message (str "Invalid OAuth nonce or timestamp values")}]))
+
+(defn- validate-signature-method
+  "Check if the oauth_signature_method parameter is one of the supported
+  signature methods, returning an error if the requested method is not
+  supported."
+  [params]
+  (let [method (params "oauth_signature_method")]
+    (if-not (contains? signature-checkers (str/lower-case method))
+      [false {:error :unsupported-signature-method
+              :status 400
+              :message (str "Unsupported signature method: " method)}])))
+
+(defn- validate-signature
+  "Validates the `oauth_signature` for the request. Returns an error if the
+  signature is invalid, or a map with the extracted OAuth credentials if it's
+  valid."
+  [request params consumer-secret-fn token-secret-fn]
+  (let [base-string (sig/signature-base request)
+        consumer-key (params "oauth_consumer_key")
+        consumer-secret (consumer-secret-fn consumer-key)
+        token (params "oauth_token")
+        token-secret (token-secret-fn token)
+        sig-checker (signature-checkers (str/lower-case (params "oauth_signature_method")))
+        request-sig (codec/percent-decode (params "oauth_signature"))
+        computed-sig (sig-checker base-string consumer-secret token-secret)]
+    (if (= request-sig computed-sig)
+      [true {:oauth-consumer-key consumer-key
+             :oauth-token token}]
+      [false {:error :invalid-signature
+              :status 401
+              :message "Invalid OAuth signature"}])))
+
+(defn- authenticate-request
   "Tries to authenticate the `request` using OAuth.
 
-  If it succeeds, returns a vector with :ok as the first element and a
-  modified request as the second. The modified request will have the
-  additional :oauth-consumer-key and :oauth-token associated.
+  If it succeeds, returns a vector with `true` as the first element and the
+  extracted OAuth data as the second.  If the authentication fails, returns
+  a vector with `false` as the first element and the error details as the
+  second. See the documentation for `wrap-oauth` for more details about"
+  [request consumer-secret-fn token-secret-fn nonce-validator-fn]
+  (let [params (sig/oauth-params request)]
+    (or (validate-required-params params)
+        (validate-nonce params nonce-validator-fn)
+        (validate-signature-method params)
+        (validate-signature request params consumer-secret-fn token-secret-fn))))
 
-  If the authentication fails, returns a vector with :ok as the first element
-  and a ring response map as the second. The :status key will be set to 400 or
-  401 depending on the type of the error and :body will be set to the error
-  message."
-  [request {:keys [consumer-secret-fn token-secret-fn]}]
-  (let [params (sig/oauth-params request)
-        base (sig/signature-base request)
-        token-secret-fn (or token-secret-fn (constantly nil))]
-    (if (required-params-missing? params)
-      [:error {:status 400 :body "Missing OAuth parameter"}]
+(defn default-error-handler
+  "Default OAuth error handler.
 
-      (let [signature-method (get params "oauth_signature_method")
-            signature-checker (get signature-checkers (str/lower-case signature-method))]
-        (if (nil? signature-checker)
-          [:error {:status 400 :body "Unsupported signature method"}]
+  Interrupts request processing, immediately returning an appropriate error
+  response. Use this if you don't want to handle authentication errors
+  yourself, and assume that all requests that reach the wrapped handler are
+  properly authenticated."
+  [_ _ error]
+  {:status (:status error)
+   :headers {"Content-Type" "text/plain; charset=utf8"}
+   :body (:message error)})
 
-          (let [base-string (sig/signature-base request)
-                consumer-key (get params "oauth_consumer_key")
-                consumer-secret (consumer-secret-fn consumer-key)
-                token (get params "oauth_token")
-                token-secret (token-secret-fn token)
-                request-sig (codec/percent-decode (get params "oauth_signature"))
-                computed-sig (signature-checker base-string consumer-secret token-secret)]
-            (if (= request-sig computed-sig)
-              [:ok (merge request
-                          {:oauth-consumer-key consumer-key
-                           :oauth-token        token})]
+(defn passthrough-error-handler
+  "OAuth error handler allowing the request to proceed.
 
-              [:error {:status 401 :body "Invalid credentials"}])))))))
+  The authentication error details will be available to the handler under the
+  `:oauth-error` key in the request map. Use this if you want to let the
+  wrapped handler deal with authentication errors."
+  [handler request error]
+  (handler (assoc request {:oauth-error error})))
 
-(defn wrap-oauth-provider
-  "Middleware to verify OAuth parameters in the request. Accepts a number of
-  options that control the verification:
+(defn default-nonce-validator
+  "Default implementation of the nonce validation function. This
+  implementation doesn't really check the nonce value and instead
+  only rejects timestamp values older than one hour."
+  [_ timestamp _ _]
+  (try
+    (let [time (-> timestamp Long/parseLong (* 1000))
+          one-hour-ago (- (System/currentTimeMillis) 3600000)]
+      (> time one-hour-ago))
+    (catch NumberFormatException _ false)))
+
+(defn- config-error
+  "Builds a ring response map for a 500 HTTP code and an error message when
+  the `wrap-oauth` middleware is not configured correctly."
+  [message]
+  {:status 500
+   :headers {"Content-Type" "text/plain; charset=utf8"}
+   :body (str "`wrap-oauth` middleware improperly configured: " message)})
+
+(def ^:private default-options
+  {:token-secret-fn (constantly nil)
+   :error-handler-fn default-error-handler
+   :nonce-validator-fn default-nonce-validator})
+
+(defn wrap-oauth
+  "Middleware to verify OAuth parameters in the request.
+
+  Accepts a number of options that controls authentication:
 
     :consumer-secret-fn - a function to lookup the shared secret for a given
                           consumer key. The function takes the consumer key
                           as its only parameter and should return the
                           corresponding consumer secret, or nil if it can't
-                          be found. This option is mandatory, and an exception
-                          will be thrown if it's not provided.
+                          be found. This option is required.
 
     :token-secret-fn    - a function to lookup the shared secret for a given
-                          token. The secret takes the token as its only
-                          parameter and should return the token secret, or nil
-                          if it can't be found. If this option isn't provided,
-                          the middleware will try to verify requests using only
-                          the consumer secret (0-legged OAuth)."
-  [handler & {:keys [consumer-secret-fn token-secret-fn] :as opts}]
-  (if consumer-secret-fn
-    (fn [request]
-      (let [[outcome request-or-response] (do-oauth request opts)]
-        (if (= :ok outcome)
-          (handler request-or-response)
-          request-or-response)))))
+                          token. It takes the token as its only parameter and
+                          should return the token secret, or nil if it can't
+                          be found. If this option isn't provided, the
+                          middleware will try to verify requests using only
+                          the consumer secret (0-legged OAuth).
+
+    :error-handler-fn   - a function to handle authentication errors. This
+                          function is called with the original handler, the
+                          current request and an error details map as
+                          parameters, if there's an authentication error in
+                          the request. The default is the
+                          `default-error-handler` function in this namespace,
+                          that responds immediately with an error. You can
+                          also use `passthrough-error-handler` or your own
+                          function.
+
+    :nonce-validator-fn - a function to validate nonce and timestamp values
+                          for an authentication request. The function should
+                          take four parameters: the nonce, timestamp, consumer
+                          key and token. This function is only called after
+                          the request has been checked for required OAuth
+                          parameters, so the first three are guaranteed to be
+                          non-nil. All values are strings. The default value
+                          for this option is the `default-nonce-validator`,
+                          that checks if timestamps are at most from one hour
+                          ago.
+
+  If this middleware is not correctly configured (right now this means a
+  missing :consumer-secret-fn option), all requests that pass through it will
+  return a 500 response."
+  [handler & {:as opts}]
+  (let [opts (merge default-options opts)
+        {:keys [consumer-secret-fn token-secret-fn error-handler-fn nonce-validator-fn]} opts]
+    (if consumer-secret-fn
+      (fn [request]
+        (let [[success details] (authenticate-request request
+                                                      consumer-secret-fn
+                                                      token-secret-fn
+                                                      nonce-validator-fn)]
+          (if success
+            (handler (merge request details))
+            (error-handler-fn handler request details))))
+      (fn [_]
+        (config-error ":consumer-secret-fn option is required.")))))
--- test/oauth/t_middleware.clj
+++ test/oauth/t_middleware.clj
@@ -1,79 +1,96 @@
 (ns oauth.t-middleware
   (use midje.sweet)
-  (require [oauth.middleware :refer [do-oauth wrap-oauth-provider]]))
+  (require [oauth.middleware :refer [default-nonce-validator wrap-oauth]]))
 
 (def valid-request
   {:request-method :post
-   :scheme         :https
-   :server-name    "api.twitter.com"
-   :server-port    443
-   :uri            "/1/statuses/update.json"
-   :params         {"include_entities" "true"
-                    "status"           "Hello Ladies + Gentlemen, a signed OAuth request!"}
-   :headers        {"authorization"
-                     (str "OAuth oauth_consumer_key=\"xvz1evFS4wEEPTGEFPHBog\","
-                          "oauth_nonce=\"kYjzVBB8Y0ZFabxSWbWovY3uYSQ2pTgmZeNu2VS4cg\","
-                          "oauth_signature_method=\"HMAC-SHA1\","
-                          "oauth_timestamp=\"1318622958\","
-                          "oauth_token=\"370773112-GmHxMAgYyLbNEtIKZeRNFsMKPR9EyMZeS9weJAEb\","
-                          "oauth_version=\"1.0\","
-                          "oauth_signature=\"tnnArxj06cWHq44gCs1OSKk%2FjLY%3D\"")}})
+   :scheme :https
+   :server-name "api.twitter.com"
+   :server-port 443
+   :uri "/1/statuses/update.json"
+   :params {"include_entities" "true"
+            "status" "Hello Ladies + Gentlemen, a signed OAuth request!"}
+   :headers {"authorization"
+             (str "OAuth oauth_consumer_key=\"xvz1evFS4wEEPTGEFPHBog\","
+                  "oauth_nonce=\"kYjzVBB8Y0ZFabxSWbWovY3uYSQ2pTgmZeNu2VS4cg\","
+                  "oauth_signature_method=\"HMAC-SHA1\","
+                  "oauth_timestamp=\"1318622958\","
+                  "oauth_token=\"370773112-GmHxMAgYyLbNEtIKZeRNFsMKPR9EyMZeS9weJAEb\","
+                  "oauth_version=\"1.0\","
+                  "oauth_signature=\"tnnArxj06cWHq44gCs1OSKk%2FjLY%3D\"")}})
 
 (def missing-params-request
   {:request-method :post
-   :scheme         :https
-   :server-name    "api.twitter.com"
-   :server-port    443
-   :uri            "/1/statuses/update.json"
-   :params         {"include_entities" "true"
-                    "status"           "Hello Ladies + Gentlemen, a signed OAuth request!"}
-   :headers        {"authorization"
-                     (str "OAuth oauth_consumer_key=\"xvz1evFS4wEEPTGEFPHBog\","
-                          "oauth_timestamp=\"1318622958\","
-                          "oauth_token=\"370773112-GmHxMAgYyLbNEtIKZeRNFsMKPR9EyMZeS9weJAEb\","
-                          "oauth_version=\"1.0\","
-                          "oauth_signature=\"tnnArxj06cWHq44gCs1OSKk%2FjLY%3D\"")}})
+   :scheme :https
+   :server-name "api.twitter.com"
+   :server-port 443
+   :uri "/1/statuses/update.json"
+   :params {"include_entities" "true"
+            "status" "Hello Ladies + Gentlemen, a signed OAuth request!"}
+   :headers {"authorization"
+             (str "OAuth oauth_consumer_key=\"xvz1evFS4wEEPTGEFPHBog\","
+                  "oauth_timestamp=\"1318622958\","
+                  "oauth_token=\"370773112-GmHxMAgYyLbNEtIKZeRNFsMKPR9EyMZeS9weJAEb\","
+                  "oauth_version=\"1.0\","
+                  "oauth_signature=\"tnnArxj06cWHq44gCs1OSKk%2FjLY%3D\"")}})
 
 (def invalid-signature-request
   {:request-method :post
-   :scheme         :https
-   :server-name    "api.twitter.com"
-   :server-port    443
-   :uri            "/1/statuses/update.json"
-   :params         {"include_entities" "true"
-                    "status"           "Hello Ladies + Gentlemen, a signed OAuth request!"}
-   :headers        {"authorization"
-                     (str "OAuth oauth_consumer_key=\"xvz1evFS4wEEPTGEFPHBog\","
-                          "oauth_nonce=\"kYjzVBB8Y0ZFabxSWbWovY3uYSQ2pTgmZeNu2VS4cg\","
-                          "oauth_signature_method=\"HMAC-SHA1\","
-                          "oauth_timestamp=\"1318622958\","
-                          "oauth_token=\"370773112-GmHxMAgYyLbNEtIKZeRNFsMKPR9EyMZeS9weJAEb\","
-                          "oauth_version=\"1.0\","
-                          "oauth_signature=\"blablabla\"")}})
+   :scheme :https
+   :server-name "api.twitter.com"
+   :server-port 443
+   :uri "/1/statuses/update.json"
+   :params {"include_entities" "true"
+            "status" "Hello Ladies + Gentlemen, a signed OAuth request!"}
+   :headers {"authorization"
+             (str "OAuth oauth_consumer_key=\"xvz1evFS4wEEPTGEFPHBog\","
+                  "oauth_nonce=\"kYjzVBB8Y0ZFabxSWbWovY3uYSQ2pTgmZeNu2VS4cg\","
+                  "oauth_signature_method=\"HMAC-SHA1\","
+                  "oauth_timestamp=\"1318622958\","
+                  "oauth_token=\"370773112-GmHxMAgYyLbNEtIKZeRNFsMKPR9EyMZeS9weJAEb\","
+                  "oauth_version=\"1.0\","
+                  "oauth_signature=\"blablabla\"")}})
 
 (def consumer-secret-fn (constantly "kAcSOqF21Fu85e7zjz7ZN2U4ZRhfV3WpwPAoE3Z7kBw"))
 
 (def token-secret-fn (constantly "LswwdoUaIvS8ltyTt5jkRh4J50vUPVVHtR2YPi5kE"))
 
-(fact "`do-auth` should return :ok for a correctly signed request"
-  (do-oauth valid-request
-            {:consumer-secret-fn consumer-secret-fn
-             :token-secret-fn    token-secret-fn})
-  => (has-prefix [:ok]))
+(let [validator #(default-nonce-validator nil % nil nil)
+      minutes-from-now #(-> (System/currentTimeMillis) (+ (* % 60000)) str)]
+  (fact "`default-nonce-validator` rejects old timestamps"
+    (validator "137131201") => false)
 
-(let [handler (fn [req] {:status 200 :body "IT WORKS!"})
-      wrapped (wrap-oauth-provider handler
-                                   :consumer-secret-fn consumer-secret-fn
-                                   :token-secret-fn token-secret-fn)]
+  (fact "`default-nonce-validator` rejects invalid timestamps"
+    (validator "abcde") => false)
+
+  (fact "`default-nonce-validator` accepts a timestamp from 30 minutes ago"
+    (let [timestamp (minutes-from-now -10)]
+      (validator timestamp) => true))
+
+  (fact "`default-nonce-validator` accepts a timestamp 10 minutes from now"
+    (let [timestamp (minutes-from-now 10)]
+      (validator timestamp) => true)))
+
+(fact
+  "`wrap-oauth` always returns 500 if it's improperly configured"
+  (let [handler (fn [req] {:status 200 :body "success"})
+        wrapped (wrap-oauth handler)]
+    (wrapped valid-request) => (contains {:status 500})))
+
+(let [handler (fn [req] {:status 200 :body "success"})
+      wrapped (wrap-oauth handler
+                          :consumer-secret-fn consumer-secret-fn
+                          :token-secret-fn token-secret-fn
+                          :nonce-validator-fn (constantly true))]
 
   (fact
-    "`wrap-oauth-provider` lets a correctly signed request proceed."
-        (wrapped valid-request) => (contains {:status 200}))
+    "`wrap-oauth` lets a correctly signed request proceed."
+    (wrapped valid-request) => (contains {:status 200}))
 
   (fact
-    "`wrap-oauth-provider complains about missing parameters."
+    "`wrap-oauth complains about missing parameters."
     (wrapped missing-params-request) => (contains {:status 400}))
 
   (fact
-    "`wrap-oauth-provider complains about invalid signatures."
+    "`wrap-oauth complains about invalid signatures."
     (wrapped invalid-signature-request) => (contains {:status 401})))
